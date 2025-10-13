fd3635bb5745dd04edc52f1d05d7cb529d145098
mii man <136441553+mii-man@users.noreply.github.com>
2025-10-13 11:02:28 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,45 @@
 #include <3ds.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  gfxInitDefault()
-  consoleInit(GFX_TOP, NULL);
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
 
-printf("AuroraChat\n");
-printf("Press START to exit.\n");
-printf("Press B to say something\n");
+    printf("AuroraChat\n");
+    printf("Press START to exit.\n");
+    printf("Press B to say something\n");
 
-int ret = socInit(buffer, bufferSize);
-    if ret == 0 {
-      printf("Networking initialized!\n")
-        } else {
-      printf("socInit() falied: %d\n", ret);
-    
+    const int bufferSize = 0x20000; // 128KB
+    void* buffer = malloc(bufferSize);
+    if (!buffer) {
+        printf("Failed to allocate memory for networking.\n");
+        goto cleanup_gfx;
+    }
 
-while (aptMainLoop())
-  hidScanInput();
-u32 kDown = hidKeysDown();
+    int ret = socInit(buffer, bufferSize);
+    if (ret == 0) {
+        printf("Networking initialized!\n");
+    } else {
+        printf("socInit() failed: %d\n", ret);
+        free(buffer);
+        goto cleanup_gfx;
+    }
 
-if (kDown & KEY_START) break;
+    while (aptMainLoop()) {
+        hidScanInput();
+        u32 kDown = hidKeysDown();
 
-gspWaitForVBlank();
-  }
-  socExit();
-  gfxExit();
-  return 0;
+        if (kDown & KEY_START)
+            break;
+
+        gspWaitForVBlank();
+    }
+
+    socExit();
+    free(buffer);
+
+cleanup_gfx:
+    gfxExit();
+    return 0;
 }
